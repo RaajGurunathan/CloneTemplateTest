4133f2686dbee9c5930194d7e1e9a0c39d0d11c0
Rajkumar <rajkumar.gurunathan90@gmail.com>
2024-07-13 21:35:04 +0530

    Test

--- update_submodule.sh
+++ update_submodule.sh
@@ -8,7 +8,7 @@ if ! git diff --quiet --exit-code;
 then
 git add .
 git commit -m "Update submodule to latest commit"
-if git push origin main;
+if git push -u origin main;
 	echo "Submodule core is updated successfully"
 else
 	echo "Failed to push in Core"
