969a5f65f0680360f48778c4af110a04c7e8f30a
Rajkumar <rajkumar.gurunathan90@gmail.com>
2024-07-13 22:52:12 +0530

    Update submodule to latest commit

--- update_submodule.sh
+++ update_submodule.sh
@@ -4,11 +4,15 @@ git submodule foreach --recursive
 cd Core
 git fetch origin
 git merge origin/main
-git add .
-git commit -m "Update submodule to latest commit"
-if git push origin main; then
-	echo "Submodule core is updated successfully"
-else
-	echo "Failed to push in Core"
-fi
 cd ..
+if ! git diff --qiet --exit-code; then
+	git add .
+	git commit -m "Update submodule to latest commit"
+	if git push origin main; then
+		echo "Submodule core is updated successfully"
+	else
+		echo "Failed to push in Core"
+	fi
+else
+	echo "No Changes in the Core folder"
+fi
