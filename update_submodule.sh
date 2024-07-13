431c0742a0edf4e23ebaf2bda3f2621556614fef
Rajkumar <rajkumar.gurunathan90@gmail.com>
2024-07-13 22:28:00 +0530

    file

--- update_submodule.sh
+++ update_submodule.sh
@@ -2,13 +2,18 @@
 cd e/Git/Project
 git submodule foreach --recursive
 cd Core
+git status
 git fetch origin
 git merge origin/main
+git status
 git add .
+git status
 git commit -m "Update submodule to latest commit"
+git status
 if git push origin main; then
 	echo "Submodule core is updated successfully"
 else
 	echo "Failed to push in Core"
 fi
+git status
 cd ..
